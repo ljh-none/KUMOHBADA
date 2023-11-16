bf82eb724e98d2c144a599e70656a79ea62dd889
ljh09-none <ljh09210921@gmail.com>
2023-11-16 19:02:03 +0900

    네이밍 변경 mystatefulpage -> MainPage 지역 선택용 팝업메뉴 코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 //////페이지 이동을 위한 상수 및 함수//////////////////////////////////////////////
 const String HOMESUB = "homesub";
@@ -12,145 +10,8 @@ const String CATEGORYSUB = "categorysub";
 const String ALERTSUB = "alertsub";
 const String SEARCHSUB = "searchsub";
 
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyStatefulPage(),
-    );
-  }
-}
-
-class MyStatefulPage extends StatefulWidget {
-  const MyStatefulPage({super.key});
-
-  @override
-  _MyStatefulPageState createState() => _MyStatefulPageState();
-}
-
-class _MyStatefulPageState extends State<MyStatefulPage> {
-  int _currentIndex = 0;
-  String? selectedLocation = '양호동';
-
-  final List<String> availableLocations = [
-    '양호동',
-    '서울',
-    '부산',
-    '대구',
-    '인천',
-    // 다른 지역을 추가할 수 있습니다.
-  ];
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  final List<Widget> _tabs = [
-    const HomeTabContent(),
-    const ChatTabContent(),
-    const ProfileTabContent(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: GestureDetector(
-          onTap: () {
-            _showLocationMenu(context);
-          },
-          child: Row(
-            children: [
-              Text(
-                selectedLocation!,
-                style: const TextStyle(color: Colors.black),
-              ),
-              const Icon(
-                Icons.keyboard_arrow_up,
-                color: Colors.black,
-              ),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.menu, color: Colors.black),
-            onPressed: () {
-              // 목록 버튼을 눌렀을 때 수행할 동작을 추가
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.search, color: Colors.black),
-            onPressed: () {
-              gotoSub(context,SEARCHSUB,selectedLocation);
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.notifications, color: Colors.black),
-            onPressed: () {
-              gotoSub(context,ALERTSUB);
-            },
-          ),
-        ],
-      ),
-      body: _tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: '채팅',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '내 정보',
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _showLocationMenu(BuildContext context) {
-    final RenderBox renderBox = context.findRenderObject() as RenderBox;
-    final Offset offset = renderBox.localToGlobal(Offset.zero);
-
-    showMenu<String>(
-      context: context,
-      position: RelativeRect.fromLTRB(
-        offset.dx,
-        offset.dy + kToolbarHeight + 5.0, // 아래에 여백 추가
-        offset.dx + renderBox.size.width,
-        offset.dy + kToolbarHeight + 5.0 + renderBox.size.height, // 아래에 여백 추가
-      ),
-      items: availableLocations.map((location) {
-        return PopupMenuItem<String>(
-          value: location,
-          child: Text(location),
-        );
-      }).toList(),
-    ).then((location) {
-      if (location != null) {
-        setState(() {
-          selectedLocation = location;
-        });
-      }
-    });
-  }
-}
-
-Function gotoSub = (BuildContext context, String cls, [String? selectedLocation]) {
+Function gotoSub =
+    (BuildContext context, String cls, [String? selectedLocation]) {
   switch (cls) {
     // case HOMESUB:
     //   Navigator.push(context,
@@ -173,36 +34,156 @@ Function gotoSub = (BuildContext context, String cls, [String? selectedLocation]
           context, MaterialPageRoute(builder: (context) => AlertSub()));
       break;
     case SEARCHSUB:
-       Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SearchSub(selectedLocation: selectedLocation)),
-              );
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                SearchSub(selectedLocation: selectedLocation)),
+      );
       break;
     default:
       break;
   }
 };
+////////////////////////////////////////////////////////////////////////////////
 
+var blackText = const TextStyle(color: Colors.black);
 
-class HomeTabContent extends StatelessWidget {
-  const HomeTabContent({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentBottomIndex = 0;
+  String? _selectedLocation = '양호동';
+
+  final List<String> _availableLocations = [
+    '양호동',
+    '서울',
+    '부산',
+    '대구',
+    '인천',
+    // 다른 지역을 추가할 수 있습니다.
+  ];
+
+  void _tapBottomTab(int index) {
+    setState(() {
+      _currentBottomIndex = index;
+    });
+  }
+
+  final List<Widget> _tabs = [
+    HomeTabContent(),
+    ChatTabContent(),
+    ProfileTabContent(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    //위젯 생성 함수
+    List<PopupMenuEntry<dynamic>> buildPopupMenu(BuildContext context) {
+      List<PopupMenuEntry<dynamic>> popUpList = [];
+      for (var temp in _availableLocations) {
+        popUpList.add(PopupMenuItem(value: temp, child: Text(temp)));
+      }
+      return popUpList;
+    }
+
     return Scaffold(
-      body: const Center(
-        child: Text('홈 탭 내용'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: PopupMenuButton(
+          itemBuilder: buildPopupMenu,
+          initialValue: _selectedLocation,
+          onSelected: (value) {
+            setState(() => _selectedLocation = value);
+          },
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(_selectedLocation!, style: blackText),
+              const Icon(Icons.keyboard_arrow_down, color: Colors.black),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.menu, color: Colors.black),
+            onPressed: () {
+              // 목록 버튼을 눌렀을 때 수행할 동작을 추가
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.black),
+            onPressed: () {
+              gotoSub(context, SEARCHSUB, _selectedLocation);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.notifications, color: Colors.black),
+            onPressed: () {
+              gotoSub(context, ALERTSUB);
+            },
+          ),
+        ],
+      ),
+      body: _tabs[_currentBottomIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentBottomIndex,
+        onTap: _tapBottomTab,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: '채팅'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '내 정보'),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // FAB를 눌렀을 때 수행할 동작을 여기에 추가하세요.
-          // 예를 들면, 새로운 작업을 추가하거나 화면을 열 수 있습니다.
-        },
-        backgroundColor: Colors.orange,
+        onPressed: () {},
         child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.endFloat, // FAB 위치 설정
+    );
+  }
+}
+
+class HomeTabContent extends StatefulWidget {
+  const HomeTabContent({super.key});
+
+  @override
+  State<HomeTabContent> createState() => _HomeTabContentState();
+}
+
+class _HomeTabContentState extends State<HomeTabContent> {
+  //더미데이터
+  List items = ["a", "b"];
+
+  @override
+  Widget build(BuildContext context) {
+    //위젯 빌드 함수
+    Widget? buildListView(context, index) {
+      return ListTile(title: Text(items[index]));
+    }
+
+    return ListView.separated(
+      itemCount: items.length,
+      itemBuilder: buildListView,
+      separatorBuilder: (context, index) {
+        return const Divider(color: Colors.black);
+      },
     );
   }
 }
@@ -243,12 +224,13 @@ class ProfileTabContent extends StatelessWidget {
 
 class SearchSub extends StatelessWidget {
   final String? selectedLocation;
-   const SearchSub({Key? key, required this.selectedLocation}) : super(key: key);
+  const SearchSub({Key? key, required this.selectedLocation}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // ignore: no_leading_underscores_for_local_identifiers
-    TextEditingController _controller = TextEditingController(text: '$selectedLocation 근처에서 검색');
+    TextEditingController _controller =
+        TextEditingController(text: '$selectedLocation 근처에서 검색');
 
     return Scaffold(
       appBar: AppBar(
@@ -268,11 +250,9 @@ class SearchSub extends StatelessWidget {
           ),
         ),
       ),
-
     );
   }
 }
-
 
 class AlertSub extends StatelessWidget {
   @override
